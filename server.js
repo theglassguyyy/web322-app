f15544894c186fc9b74e3023c06fde32c5e81269
Aashish Sapkota <asapkota9@myseneca.ca>
2024-10-06 15:12:05 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,38 +1,21 @@
 // server.js
 const express = require('express');
 const path = require('path');
-const storeService = require('./store-service');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Route for About page
+// Redirect the root ("/") to "/about"
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+// Serve the "/about" page
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
-
-// Route for Shop 
-app.get('/shop', (req, res) => {
-  storeService.getPublishedItems()
-    .then((data) => res.json(data))
-    .catch((err) => res.status(500).json({ message: err }));
-});
-
-// Route for Items
-app.get('/items', (req, res) => {
-  storeService.getAllItems()
-    .then((data) => res.json(data))
-    .catch((err) => res.status(500).json({ message: err }));
-});
-
-// Route for Categories
-app.get('/categories', (req, res) => {
-  storeService.getCategories()
-    .then((data) => res.json(data))
-    .catch((err) => res.status(500).json({ message: err }));
 });
 
 // Start the server
